6384676be334900e6cc80f09ed13956d56483be1
Thom-320 <166944998+Thom-320@users.noreply.github.com>
2025-09-16 20:23:09 -0500

    Fix categorical pivot with type unification, consolidate Jupyter package setup, add IRkernel setup; add RStudio project file

--- code/install_packages.R
+++ code/install_packages.R
@@ -1,14 +1,26 @@
 # Script para instalar paquetes necesarios
+# Configurar mirror de CRAN
+options(repos = c(CRAN = "https://cran.rstudio.com/"))
+
 packages <- c(
-  "tidyverse", "janitor", "skimr", "ggplot2", "ggthemes", 
-  "rstatix", "effectsize", "readr", "patchwork", "corrplot", 
-  "psych", "knitr", "kableExtra"
+  "tidyverse", "janitor", "skimr", "gt", "GGally", "broom", "gridExtra", "viridis",
+  "ggplot2", "ggthemes", "rstatix", "effectsize", "readr", "patchwork", "corrplot",
+  "psych", "knitr", "kableExtra", "rmarkdown", "IRkernel"
 )
+
+cat("Instalando paquetes necesarios...\n")
 
 # Instalar paquetes que no estén instalados
 new_packages <- packages[!(packages %in% installed.packages()[,"Package"])]
 if(length(new_packages)) {
+  cat("Paquetes a instalar:", paste(new_packages, collapse = ", "), "\n")
   install.packages(new_packages, dependencies = TRUE)
+  if ("IRkernel" %in% new_packages) {
+    cat("Registrando kernel de Jupyter para R...\n")
+    IRkernel::installspec()
+  }
+} else {
+  cat("Todos los paquetes ya están instalados.\n")
 }
 
 # Verificar instalación
